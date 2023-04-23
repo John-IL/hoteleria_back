46bb2910d6510a18ff2df69c65d93fe8e00bb005
John Inga Lapa <85035882+John-IL@users.noreply.github.com>
2023-04-22 19:15:24 -0500

    add sp and update models

--- seeder.py
+++ seeder.py
@@ -14,88 +14,91 @@ mydb = mysql.connector.connect(user=os.environ.get('DB_USER'), password=os.envir
                            host=os.environ.get('DB_HOST'),
                            database=os.environ.get('DB_NAME'))
 
-module = ()
-document = ()
-user = ()
+# module = ()
+# document = ()
+# user = ()
 
-with open('data/modules.csv', newline='', encoding='utf-8-sig', mode='r') as f:
-    reader = csv.reader(f, delimiter=';')
-    for row in reader:
-        created = Modules.objects.get_or_create(
-            name=row[1],
-            route=row[2],
-            initial=row[3],
-            icons=row[4],
-        )
+# with open('data/modules.csv', newline='', encoding='utf-8-sig', mode='r') as f:
+#     reader = csv.reader(f, delimiter=';')
+#     for row in reader:
+#         created = Modules.objects.get_or_create(
+#             name=row[1],
+#             route=row[2],
+#             initial=row[3],
+#             icons=row[4],
+#         )
 
-        module = created[0]
+#         module = created[0]
 
-with open('data/roles.csv', newline='', encoding='utf-8-sig', mode='r') as f:
-    reader = csv.reader(f, delimiter=';')
-    for row in reader:
-        created = Roles.objects.get_or_create(
-            name=row[1],
-            slug=row[2],
-            description=row[3]
-        )
+# with open('data/roles.csv', newline='', encoding='utf-8-sig', mode='r') as f:
+#     reader = csv.reader(f, delimiter=';')
+#     for row in reader:
+#         created = Roles.objects.get_or_create(
+#             name=row[1],
+#             slug=row[2],
+#             description=row[3]
+#         )
 
 
-with open('data/countries.csv', newline='', encoding='utf-8-sig', mode='r') as f:
-    reader = csv.reader(f, delimiter=';')
-    for row in reader:
-        created = StaticCountries.objects.get_or_create(
-            name=row[0],
-            iso=row[1]
-        )
+# with open('data/countries.csv', newline='', encoding='utf-8-sig', mode='r') as f:
+#     reader = csv.reader(f, delimiter=';')
+#     for row in reader:
+#         created = StaticCountries.objects.get_or_create(
+#             name=row[0],
+#             iso=row[1]
+#         )
 
-with open('data/documents.csv', newline='', encoding='utf-8-sig', mode='r') as f:
-    reader = csv.reader(f, delimiter=';')
-    for row in reader:
-        created = StaticDocumentTypes.objects.get_or_create(
-            name=row[0],
-            sunat_code=row[1]
-        )
-        document = created
+# with open('data/documents.csv', newline='', encoding='utf-8-sig', mode='r') as f:
+#     reader = csv.reader(f, delimiter=';')
+#     for row in reader:
+#         created = StaticDocumentTypes.objects.get_or_create(
+#             name=row[0],
+#             sunat_code=row[1]
+#         )
+#         document = created
 
-with open('data/user.csv', newline='', encoding='utf-8-sig', mode='r') as f:
-    reader = csv.reader(f, delimiter=';')
-    for row in reader:
-        user = UserProfile.objects.get_or_create(
-            first_name=row[1],
-            last_name=row[2],
-            country_id=row[3],
-            phone=row[4],
-            email=row[5],
-            document_type=document[0],
-            document_number=row[7],
-            role_id=row[8],
-            personal_type=1,
-            password=make_password(password=row[9])
-        )
+# with open('data/user.csv', newline='', encoding='utf-8-sig', mode='r') as f:
+#     reader = csv.reader(f, delimiter=';')
+#     for row in reader:
+#         user = UserProfile.objects.get_or_create(
+#             first_name=row[1],
+#             last_name=row[2],
+#             country_id=row[3],
+#             phone=row[4],
+#             email=row[5],
+#             document_type=document[0],
+#             document_number=row[7],
+#             role_id=row[8],
+#             personal_type=1,
+#             password=make_password(password=row[9])
+#         )
 
-with open('data/sections.csv', newline='', encoding='utf-8-sig', mode='r') as f:
-    reader = csv.reader(f, delimiter=';')
-    for row in reader:
-        section = Sections.objects.get_or_create(
-            name=row[1],
-            route=row[2],
-            is_new=row[3],
-            icon=row[4],
-            status=row[5]
-        )
-        module_section = ModuleSections.objects.get_or_create(
-            section_id=section[0].id,
-            module_id=module.id
-        )
+# with open('data/sections.csv', newline='', encoding='utf-8-sig', mode='r') as f:
+#     reader = csv.reader(f, delimiter=';')
+#     for row in reader:
+#         section = Sections.objects.get_or_create(
+#             name=row[1],
+#             route=row[2],
+#             is_new=row[3],
+#             icon=row[4],
+#             status=row[5]
+#         )
+#         module_section = ModuleSections.objects.get_or_create(
+#             section_id=section[0].id,
+#             module_id=module.id
+#         )
 
-        user_section = UserSections.objects.get_or_create(
-            module_section = module_section[0],
-            user_id = user[0].id
-        )
+#         user_section = UserSections.objects.get_or_create(
+#             module_section = module_section[0],
+#             user_id = user[0].id
+#         )
 
 ## read and execute store procedure
 
-archivos_sql = ['sp/1_login.sql','sp/2_get_users.sql', 'sp/3_insert_user.sql', 'sp/4_update_user.sql']
+archivos_sql = ['sp/1_login.sql','sp/2_get_users.sql', 'sp/3_insert_user.sql', 'sp/4_update_user.sql', 'sp/5_insert_client.sql','sp/6_update_client.sql',
+                'sp/7_insert_promotion.sql','sp/8_update_promotion.sql','sp/9_insert_room_detail.sql', 'sp/10_get_clients.sql','sp/11_get_promotions.sql',
+                'sp/12_update_room_detail.sql','sp/13_get_room_categories.sql','sp/14_get_reserves.sql','sp/15_insert_reserve.sql','sp/16_insert_testimonial.sql',
+                'sp/17_update_testimonial.sql']
 cursor = mydb.cursor()
 
 for archivo in archivos_sql:
---
+++ sp/15_insert_reserve.sql
@@ -0,0 +1,45 @@
+CREATE PROCEDURE insert_reserve(
+            IN _table json,
+            IN _detail json)
+            
+BEGIN
+	DECLARE error_msg TEXT DEFAULT '';
+	DECLARE error_code INT;
+	DECLARE i int default 0; 
+  	DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+  	
+  	BEGIN
+      GET DIAGNOSTICS CONDITION 1 error_msg = MESSAGE_TEXT;
+      SELECT JSON_OBJECT("code",500,"message",error_msg) response;
+    END;
+	
+  	start transaction;
+	       
+		if(json_length(_detail) <= 0) then 
+		
+			select JSON_OBJECT("code",500,"message","data cannot be null") response;
+			rollback;
+		
+		else
+			
+ 			insert into api_reserve (reserve_date,observation,client_id,payment_method_id,personal,created_at) 
+ 						value (_table->>'$.reserve_date',_table->>'$.observation',_table->>'$.client',_table->>'$.payment_method',_table->>'$.personal',now());
+ 			
+ 			 set @insert_id = @@identity;
+                        
+			 while i < JSON_LENGTH(_detail) do
+			    set @start_date = JSON_UNQUOTE(JSON_EXTRACT(_detail,CONCAT('$[',i,'].start_date')));
+             	set @end_date = JSON_EXTRACT(_detail,CONCAT('$[',i,'].end_date'));
+             	set @room = JSON_UNQUOTE(JSON_EXTRACT(_detail,CONCAT('$[',i,'].room')));
+			   
+			   	insert into api_reservedatedetail (start_date,end_date,reserve_id,room_id) value(@start_date,@end_date,@insert_id,@room);
+			   	select i+1 into i;
+			   	
+             end while;
+		end if;
+	
+	commit;
+	
+	select JSON_OBJECT("code",200,"message","transaction successful") response;
+         
+  END
